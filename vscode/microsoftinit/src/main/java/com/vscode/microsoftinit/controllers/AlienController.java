e5a37840d2f8b04f04901f99656d823dd23e2ee0
Ryan McCoppin <rrmhearts@gmail.com>
2020-03-12 15:28:42 -0400

    RESTful GET alien requests.

--- vscode/microsoftinit/src/main/java/com/vscode/microsoftinit/controllers/AlienController.java
+++ vscode/microsoftinit/src/main/java/com/vscode/microsoftinit/controllers/AlienController.java
@@ -3,8 +3,11 @@ package com.vscode.microsoftinit.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -73,6 +76,22 @@ public class AlienController
         return aliens;        
     }
 
+    /* This will return data to browser*/
+    @RequestMapping("/aliens")
+    @ResponseBody // Returning data, not view name.
+	public String aliens()
+	{
+        // not a view, thus we need ResponseBody
+		return repo.findAll().toString();
+    }
+    
+    @RequestMapping("/alien/{aid}")
+    @ResponseBody // Returning data, not view name.
+	public String alien_aid(@PathVariable("aid")int aid)
+	{
+		return repo.findById(aid).toString();
+    }
+    
     /*
 	@DeleteMapping("/alien/{aid}")
 	public String deleteAlien(@PathVariable int aid)
@@ -81,7 +100,14 @@ public class AlienController
 		repo.delete(a);
 		return "deleted";
 	}
-	
+    
+    @GetMapping(path="/aliens")
+	public List<Alien> getAliens()
+	{
+		return repo.findAll();
+			
+    }
+
 	@PostMapping(path="/alien",consumes= {"application/json"})
 	public Alien addAlien(@RequestBody Alien alien)
 	{
